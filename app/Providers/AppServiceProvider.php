8bd7eba4de4d107326798346ba456121bbb6408b
Nuris Akbar <nuris.akbar@gmail.com>
2021-09-11 22:42:16 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $setting = Setting::first();
-        View::share('setting', $setting);
+        // $setting = Setting::first();
+        // View::share('setting', $setting);
     }
 }
