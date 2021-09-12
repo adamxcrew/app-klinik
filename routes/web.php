b223c664d88af597b5311e8408abd5e87aee9097
Nuris Akbar <nuris.akbar@gmail.com>
2021-09-12 23:31:40 +0700

    init

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('test', function () {
-    dd(generateKodePendaftaran());
+    return redirect('login');
 });
 Auth::routes();
 Route::middleware(['auth'])->group(function () {
