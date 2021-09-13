d7ef27ce1e08bb46f0a2ee61044e50819a2c802f
AldianBaim <aldispartan12@gmail.com>
2021-09-13 16:37:05 +0700

    Data master agama dan pendidikan

---
+++ app/Models/Agama.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Agama extends Model
+{
+    protected $table = "agama";
+
+    protected $fillable = ['agama'];
+}
