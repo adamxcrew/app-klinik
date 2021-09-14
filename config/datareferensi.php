932e13335e59ab787df1c0001d00787a7b97f877
Nuris Akbar <nuris.akbar@gmail.com>
2021-09-14 11:39:15 +0700

    Create datareferensi.php

---
+++ config/datareferensi.php
@@ -0,0 +1,10 @@
+<?php
+
+// kelompok pegawai
+return [
+    ['kelompok_pegawai'     =>   ['a'=>'b']],
+
+    ['agama'                =>              ['a'=>'b']],
+
+    ['jenjang_pendidikan'   =>['a'=>'b']]
+];
