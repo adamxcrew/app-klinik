6ca5718e73a554e77aa0829a6c237e6c85eb9c1b
Nuris Akbar <nuris.akbar@gmail.com>
2021-09-14 11:51:16 +0700

    Update datareferensi.php

--- config/datareferensi.php
+++ config/datareferensi.php
@@ -1,10 +1,31 @@
 <?php
 
-// kelompok pegawai
+
 return [
-    ['kelompok_pegawai'     =>   ['a'=>'b']],
 
-    ['agama'                =>              ['a'=>'b']],
 
-    ['jenjang_pendidikan'   =>['a'=>'b']]
+
+    // kelompok pegawai
+    'kelompok_pegawai'     =>   [
+                                    'dokter'                => 'Dokter',
+                                    'perawat'               => 'Perawat',
+                                    'administrasi'          => 'Administrasi',
+                                    'penunjang_medis'       => 'Penunjang Medis',
+                                    'security'              => 'Security',
+                                    'non_medis'             => 'Non Medis',
+                                    'bidan'                 => 'Bidan',
+                                    'dokter_radioterapi'    => 'Dokter Radioterapi',
+                                    'farmasi'               => 'Farmasi',
+                                    'dokter_luar'           => 'Dokter Luar',
+                                    'terapis'               => 'Terapis',
+                                    'supir'                 => 'Supir',
+                                    'radiografer'           => 'Radiografer',
+                                    'direkur'               => 'Direkur',
+                                    'keuangan'              => 'Keuangan',
+    ]
+    ,
+    'agama'                 => [
+                                    'islam'                 =>  'Islam'
+
+    ]
 ];
