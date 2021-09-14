7965c92a4044e0a6b8359b9dd7a18e9d2fcf685c
AldianBaim <aldispartan12@gmail.com>
2021-09-14 13:33:51 +0700

    Membuat CRUD data perusahaan asuransi

---
+++ resources/views/perusahaan-asuransi/create.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+@section('title','Tambah Perusahaan Asuransi')
+@section('content')
+<div class="content-wrapper">
+  <section class="content-header">
+    <h1>
+      Kelola Perusahaan Asuransi
+      <small>Tambah Perusahaan Asuransi</small>
+    </h1>
+    <ol class="breadcrumb">
+      <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+      <li class="active">Dashboard</li>
+    </ol>
+  </section>
+
+  <section class="content">
+    <div class="row">
+      <div class="col-xs-12">
+        <div class="box">
+          <div class="box-body">
+            {!! Form::open(['route'=>'asuransi.store','class'=>'form-horizontal']) !!}
+            @include('validation_error')
+            @include('perusahaan-asuransi.form')
+            {!! Form::close() !!}
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</div>
+@endsection
