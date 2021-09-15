6b328d9d1c188ed9008d1f65ffc337c7b37078c3
AldianBaim <aldispartan12@gmail.com>
2021-09-15 14:46:21 +0700

    Fixing edit dan show data pasien

--- resources/views/pasien/edit.blade.php
+++ resources/views/pasien/edit.blade.php
@@ -18,7 +18,7 @@
           <div class="col-xs-12">
             <div class="box">
               <div class="box-body">
-                  {!! Form::model($pasien,['route'=>['pasien.update',$pasien->id],'method'=>'PUT','class'=>'form-horizontal']) !!}
+                  {!! Form::model($pasien,['route'=>['pasien.update',$pasien->id],'method'=>'PUT']) !!}
                   @include('validation_error')
                   @include('pasien.form')
                   {!! Form::close() !!}
