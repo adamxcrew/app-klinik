d76076a981f2ed0cd033e9a6fc08c69c5d73f082
Nuris Akbar <nuris.akbar@gmail.com>
2021-09-18 01:59:26 +0700

    change column

---
+++ database/migrations/2021_09_17_185521_change_column_tanggal_selesai_null.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeColumnTanggalSelesaiNull extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('surat_sehat_sakit', function (Blueprint $table) {
+            $table->date('tanggal_selesai')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('surat_sehat_sakit', function (Blueprint $table) {
+            //
+        });
+    }
+}
