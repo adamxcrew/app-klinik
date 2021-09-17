3f32c46737bdd59095b6f75306d29411dbb92d42
Nuris Akbar <nuris.akbar@gmail.com>
2021-09-17 22:28:30 +0700

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -30,7 +30,7 @@
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <a href="../../index2.html"><b>Aplikasi</b> Klinik</a>
+    <a href="../../index2.html"><b>Aplikasi</b> Klinik Nurdin Wahid</a>
   </div>
   <!-- /.login-logo -->
   <div class="login-box-body">
