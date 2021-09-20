ff30330593d5dd1c9c6575e01342d20455124475
AldianBaim <aldispartan12@gmail.com>
2021-09-20 15:43:56 +0700

    Menambahkan Komponen Gaji

--- app/Http/Controllers/PegawaiController.php
+++ app/Http/Controllers/PegawaiController.php
@@ -2,11 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use DataTables;
 use App\Models\Pegawai;
+use Illuminate\Http\Request;
+use App\Models\KomponenGaji;
 use App\Http\Requests\PegawaiStoreRequest;
-use App\Models\Agama;
 
 class PegawaiController extends Controller
 {
@@ -28,10 +28,11 @@ class PegawaiController extends Controller
         if ($request->ajax()) {
             return DataTables::of(Pegawai::all())
                 ->addColumn('action', function ($row) {
-                    $btn = \Form::open(['url' => 'pegawai/' . $row->id, 'method' => 'DELETE', 'style' => 'float:right;margin-right:5px']);
+                    $btn = \Form::open(['url' => 'pegawai/' . $row->id, 'method' => 'DELETE', 'style' => 'float:right;margin-left:-50px']);
                     $btn .= "<button type='submit' class='btn btn-danger btn-sm'><i class='fa fa-trash' aria-hidden='true'></i></button>";
                     $btn .= \Form::close();
-                    $btn .= '<a class="btn btn-danger btn-sm" href="/pegawai/' . $row->id . '/edit"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a> ';
+                    $btn .= '<a style="margin-left:20px" class="btn btn-danger btn-sm" href="/pegawai/' . $row->id . '/edit"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a> ';
+                    $btn .= '<a style="margin-left:8px" class="btn btn-danger btn-sm" href="/pegawai/' . $row->id . '"><i class="fa fa-eye" aria-hidden="true"></i></a> ';
                     return $btn;
                 })
                 ->rawColumns(['action'])
@@ -73,8 +74,9 @@ class PegawaiController extends Controller
      */
     public function show($id)
     {
-        $data['akun'] = Akun::findOrFail($id);
-        return view('akun.show', $data);
+        $data['pegawai'] = Pegawai::findOrFail($id);
+        $data['komponen_gaji'] = KomponenGaji::pluck('nama_komponen', 'id');
+        return view('pegawai.show', $data);
     }
 
     /**
