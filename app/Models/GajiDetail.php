69fff3c1c20cb8c679b6a96cf2cf636f67c76059
AldianBaim <aldispartan12@gmail.com>
2021-09-27 13:24:38 +0700

    Sesuaikan Slip Gaji Dengan Data Real

--- app/Models/GajiDetail.php
+++ app/Models/GajiDetail.php
@@ -6,7 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class GajiDetail extends Model
 {
-    protected $table ="gaji_detail";
+    protected $table = "gaji_detail";
 
-    protected $fillable = ['gaji_id','komponen_gaji_id','jumlah','pegawai_id'];
+    protected $fillable = ['gaji_id', 'komponen_gaji_id', 'jumlah', 'pegawai_id'];
+
+    public function komponen_gaji()
+    {
+        return $this->hasMany(KomponenGaji::class, 'id', 'komponen_gaji_id');
+    }
 }
