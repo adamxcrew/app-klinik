f0f02ae7876cf6dda499b6d5421ef10100cacf43
Nuris Akbar <nuris.akbar@gmail.com>
2021-10-09 11:09:09 +0700

    Update purchase-order-item.blade.php

--- resources/views/purchase-order/purchase-order-item.blade.php
+++ resources/views/purchase-order/purchase-order-item.blade.php
@@ -6,9 +6,9 @@
             <th scope="col">Nama Barang</th>
             <th scope="col">Jumlah</th>
             <th scope="col">Harga (PO)</th>
-            @if(!in_array($purchase_order->status_po,['selesai_po','approve_by_pimpinan']))
+            {{-- @if(!in_array($purchase_order->status_po,['selesai_po','approve_by_pimpinan'])) --}}
                 <th scope="col">Action</th>
-            @endif
+            {{-- @endif --}}
         </tr>
     </thead>
     <tbody>
@@ -19,30 +19,32 @@
             <td>{{ $row->barang->kode }}</td>
             <td>{{ $row->barang->nama_barang }}</td>
             <td>
-                @if(in_array($purchase_order->status_po,['selesai_po','approve_by_pimpinan']))
+                {{-- @if(in_array($purchase_order->status_po,['selesai_po','approve_by_pimpinan']))
                     {{ $row->qty }}
                 @else
                     <a href="#" class="editableRow" data-pk = '{{$row->id}}' data-name = 'qty'>
                         {{ $row->qty }}
                     </a>
-                @endif
+                @endif --}}
+                {{ $row->qty }}
             </td>
             <td>
-                @if(in_array($purchase_order->status_po,['selesai_po','approve_by_pimpinan']))
+                {{-- @if(in_array($purchase_order->status_po,['selesai_po','approve_by_pimpinan']))
                         @currency($row->harga)
                 @else
                     <a href="#" class="editableRow" data-pk = '{{$row->id}}' data-name = 'harga'>
                         @currency($row->harga)
                     </a>
-                @endif
+                @endif --}}
+                @currency($row->harga)
             </td>
-            @if(!in_array($purchase_order->status_po,['selesai_po','approve_by_pimpinan']))
+            {{-- @if(!in_array($purchase_order->status_po,['selesai_po','approve_by_pimpinan'])) --}}
             <td>
                 <button class="btn btn-danger btn-sm" onClick="hapus_barang({{ $row->id }})">
                     <i class="fa fa-trash"></i>
                 </button>
             </td>
-            @endif
+            {{-- @endif --}}
         </tr>
         <?php $total += $row->harga * $row->qty;?>
         @endforeach
