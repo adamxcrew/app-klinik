edf3ad7f55df6c4d23bb8153301a15dd1296ff4c
Nuris Akbar <nuris.akbar@gmail.com>
2021-10-22 11:34:44 +0700

    update login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -30,7 +30,7 @@
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <a href="/"><b>Klinik Nurdin Wahid</b></a>
+    <a href="/"><b>Klinik Nurdin Wahid 1</b></a>
   </div>
   <!-- /.login-logo -->
   <div class="login-box-body">
