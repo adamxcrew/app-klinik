f08288712f66d7f99bc39e347d890343f5c36f3d
AldianBaim <aldiansyahibrahim5@gmail.com>
2021-11-09 15:31:07 +0700

    Template laporan surat keterangan kelahiran

---
+++ resources/views/surat-keterangan-kelahiran.blade.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Surat Keterangan Kelahiran</title>
+    <style>
+        .text-center {
+            text-align: center;
+        } 
+        .mt-3 {
+            margin-top: 30px;
+        }
+    </style>
+</head>
+<body style="border: 1px solid black;padding:10px">
+
+    <div>
+        <table>
+            <tr>
+                <td><img src="https://alumniphb.net/upload/loker//download_(3)1.png" alt=""></td>
+                <td>
+                    <h3>KLINIK DR. NURDIN WAHID</h3>
+                    <p style="margin: -13px 0">Jl. HM. Ashari no.22 Cibinong-Bogor</p>
+                    <p>Telp. 021 87916739</p>
+                </td>
+            </tr>
+        </table>
+    </div>
+    <hr><hr>
+    <h4 class="text-center">SURAT KETERANGAN KELAHIRAN</h4>
+    <h5 class="text-center">NO ___/___/_____</h5>
+
+    <div class="mt-3">
+        <div style="margin-left: 20px;">
+        <p>Yang bertanda tangan dibawah ini :</p>
+            <table>
+                <tr>
+                    <td style="padding-right: 20px">Nama</td>
+                    <td >:</td>
+                    <th align="left">____________________</th>
+                </tr>   
+            </table>
+        </div>
+
+        <div style="margin-left: 20px;">
+        <p>Menerangkan bahwa telah kami tolong/rawat,</p>
+            <table>
+                <tr>
+                    <td style="padding-right: 20px">Bayi yang diberi nama</td>
+                    <td >:</td>
+                    <th align="left">______________________________</th>
+                </tr>   
+                <tr>
+                    <td style="padding-right: 20px">Jenis kelamin</td>
+                    <td >:</td>
+                    <th align="left">______________________________</th>
+                </tr>   
+                <tr>
+                    <td style="padding-right: 20px">Lahir Hari / Tanggal / Jam</td>
+                    <td >:</td>
+                    <th align="left">______________________________</th>
+                </tr>   
+                <tr>
+                    <td style="padding-right: 20px">Anak dari Nyonya</td>
+                    <td >:</td>
+                    <th align="left">______________________________</th>
+                </tr>   
+                <tr>
+                    <td style="padding-right: 20px">Nama Ayah</td>
+                    <td >:</td>
+                    <th align="left">______________________________</th>
+                </tr>   
+                <tr>
+                    <td style="padding-right: 20px">No. KTP</td>
+                    <td >:</td>
+                    <th align="left">______________________________</th>
+                </tr>   
+                <tr>
+                    <td style="padding-right: 20px">Pekerjaan</td>
+                    <td >:</td>
+                    <th align="left">______________________________</th>
+                </tr>   
+                <tr>
+                    <td style="padding-right: 20px">Alamat Rumah</td>
+                    <td >:</td>
+                    <th align="left">______________________________</th>
+                </tr>   
+                <tr>
+                    <td style="padding-right: 20px">Agama</td>
+                    <td >:</td>
+                    <th align="left">______________________________</th>
+                </tr>   
+                <tr>
+                    <td style="padding-right: 20px">No. Register Ibu</td>
+                    <td >:</td>
+                    <th align="left">______________________________</th>
+                </tr>   
+            </table>
+        </div>
+
+        <div style="margin-left: 20px;">
+            <p>Data Kelahiran :</p>
+                <table>
+                    <tr>
+                        <td style="padding-right: 20px">1. Panjang</td>
+                        <td >:</td>
+                        <td align="left">__________cm</td>
+                    </tr>   
+                    <tr>
+                        <td style="padding-right: 20px">2. Berat Badan</td>
+                        <td >:</td>
+                        <td align="left">__________gram</td>
+                    </tr>   
+                    <tr>
+                        <td style="padding-right: 20px">3. Keadaan Bayi</td>
+                        <td >:</td>
+                        <td align="left">Normal / dengan kelainan Sebutkan___________</td>
+                    </tr>   
+                    <tr>
+                        <td style="padding-right: 20px">4. Anak Ke</td>
+                        <td >:</td>
+                        <td align="left">1 / 2 / 3 / 4 / 5</td>
+                    </tr>   
+                </table>
+            </div>
+
+        {{-- <p>Demikian surat keterangan ini diberikan untuk diketahui dan dipergunakan sebagaimana mestinya.</p> --}}
+        <div style="float: left;margin-left:40px">
+            <p>&nbsp;</p>
+            <p style="margin-right:90px;margin-bottom:70px">Mengetahui Bidan Koordinator</p>
+            <p style="margin-left: 23px">___________________</p>
+            <p style="margin-left: 23px">Nama & Tanda Tangan</p>
+        </div>
+        <div style="float: right">
+            <p style="margin-right:70px;">Cibinong, _______________</p>
+            <p style="margin-left:23px;margin-bottom:70px">Bidan yang menolong</p>
+            <p style="margin-left: 23px">___________________</p>
+            <p style="margin-left: 23px">Nama & Tanda Tangan</p>
+        </div>
+    </div>
+    
+</body>
+</html>
