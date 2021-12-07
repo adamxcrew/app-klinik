6b6b18680252fb164e36cc43ce29e347bf6cd8d6
AldianBaim <aldiansyahibrahim5@gmail.com>
2021-12-07 12:07:36 +0700

    Update riwayat pelayanan

--- resources/views/pendaftaran/test.blade.php
+++ resources/views/pendaftaran/test.blade.php
@@ -5,7 +5,7 @@
     <section class="content-header">
       <h1>
         Pendaftaran
-        <small>PD-202111270004 </small>
+        <small>{{ $pendaftaran->kode }} </small>
       </h1>
       <ol class="breadcrumb">
         <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
