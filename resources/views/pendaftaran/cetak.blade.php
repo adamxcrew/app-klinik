1020cd5cd6510cd35cb4da5b5d1c9f11a2f91bdb
Nuris Akbar <nuris.akbar@gmail.com>
2022-05-24 22:11:58 +0700

    Update cetak.blade.php

--- resources/views/pendaftaran/cetak.blade.php
+++ resources/views/pendaftaran/cetak.blade.php
@@ -58,8 +58,8 @@
     <div style="text-align:center">
         <h2 style="font-size: 3em"><u>NOMOR ANTRIAN POLIKLINIK</u></h2>
         <h2 style="font-size: 4em;margin-bottom:-20px">{{ $pasien->pasien->nomor_rekam_medis }}</h2>
-        <h2 style="margin-top: -15px;font-size: 4em;margin-bottom:-20px">{{ $pasien->pasien->nama }}</h2>
-        <h4 style="margin-top: 70px;margin-bottom:-60px;font-size: 3em">NO. URUT</h4>
+        <h2 style="margin-top: -15px;font-size: 4em;margin-bottom:-10px">{{ $pasien->pasien->nama }}</h2>
+        <h4 style="margin-top: 50px;margin-bottom:-60px;font-size: 3em">NO. URUT</h4>
         <h1 style="font-size: 5em">{{ $pasien->nomor_antrian }}</h1>
         <h1 style="font-size: 4em;margin-bottom:-20px">{{ $pasien->poliklinik->nama }}</h1>
         <h1 style="font-size: 3em">{{ $pasien->dokter->name }}</h1>
