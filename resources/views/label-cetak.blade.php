dedb877ba37bb091b1fefaaa6a02517a9eace84f
Nuris Akbar <nuris.akbar@gmail.com>
2022-05-26 12:22:07 +0700

    Update label-cetak.blade.php

--- resources/views/label-cetak.blade.php
+++ resources/views/label-cetak.blade.php
@@ -7,8 +7,8 @@
     
         width: 200%;
         margin-left:-30px;
-        margin-top:-40px;
-        margin-bottom:-30px;
+        margin-top:-50px;
+        margin-bottom:-50px;
         text-transform: uppercase;
         page-break-after:always;
     }
@@ -20,10 +20,11 @@
         <b>{{ $setting->nama_instansi }} ( {{ $setting->nomor_telpon }} )</b><br>
         Apoteker : {{ Auth::user()->name }}<br>
         SIPA : 1234567890/ 123456789<br>
-        {{ $pendaftaran->pasien->nomor_rekam_medis}} - {{ $pendaftaran->pasien->nama}}
+        {{ $pendaftaran->pasien->nomor_rekam_medis}} - <b>{{ $pendaftaran->pasien->nama}}</b>
         <br>
-    {{ date("d/m/Y", strtotime($pendaftaran->pasien->tanggal_lahir))}}<br>
-    Sample Text Sample Text Sample<br>
+    <b>{{ date("d/m/Y", strtotime($pendaftaran->pasien->tanggal_lahir))}}</b><br>
+    {{ $pendaftaran->kode }}  {{ date('d/m/Y H:i')}} <br>ED : <br>
+    Sample Nama Obat 100 ML<br>
     2 x 1 diminum sesudah makan
     </p>
 </div>
