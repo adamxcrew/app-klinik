37b65cb83a2d364fb49fa8ef0c17722671112266
Nuris Akbar <nuris.akbar@gmail.com>
2022-06-30 09:44:40 +0700

    Create 2022_06_30_094237_perbaikan_view_pendaftaran.php

---
+++ database/migrations/2022_06_30_094237_perbaikan_view_pendaftaran.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class PerbaikanViewPendaftaran extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        \DB::statement("DROP view view_pendaftaran");
+        \DB::statement("create view view_pendaftaran as select na.pendaftaran_id,na.id,pa.nomor_rekam_medis,pa.nama,na.nomor_antrian,left(na.created_at,10)as tanggal,po.nomor_poli as nama_poliklinik,po.id as poliklinik_id,u.name as nama_dokter,pas.nama_perusahaan,na.status_pelayanan,na.sudah_dipanggil,na.status_pembayaran
+        from nomor_antrian as na left join pendaftaran as p on na.pendaftaran_id=p.id
+        left join pasien as pa on pa.id=p.pasien_id
+        left join poliklinik as po on po.id=na.poliklinik_id
+        left JOIN users as u on u.id=na.dokter_id
+        left join perusahaan_asuransi as pas on pas.id=na.perusahaan_asuransi_id");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
